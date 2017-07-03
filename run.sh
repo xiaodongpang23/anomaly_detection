eaafd55dd79d8b4ad11463ca42fcb054205a3d94
Xiaodong Pang <xiaodong0119@gmail.com>
2017-07-03 14:54:11 -0400

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python ./src/process_log.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json
+python ./src/anomaly_detection.py ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json
